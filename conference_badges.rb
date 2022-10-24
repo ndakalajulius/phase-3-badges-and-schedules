886db8a6da389ae1c63d1f8320e23bd32350b967
ndakalajulius <julius.ndakala@student.moringaschool>
2022-10-24 12:53:07 +0300

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+    p "Hello, my name is #{name}."
+
+end
+
+badge_maker ("julius")
+
+def batch_badge_creator([name1, name2,name3,name4])
+    
+end
+
+
+
